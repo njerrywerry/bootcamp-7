a32c4e9d79a8581ec3d5bb3e3fc9afc5968f3de0
Sylvia <sylviakieha@gmail.com>
2016-04-26 17:40:41 +0300

    sum_digits function

---
+++ day_2/sum_digits.py
@@ -0,0 +1,16 @@
+def sum_digits(A):
+    '''
+    Takes a list A, and returns
+    the sum of all the digits in the
+    list e.g. [10, 30, 45] should return
+    1 + 0 + 3 + 0 + 4 + 5 = 13
+    '''
+    total = 0
+
+    for i in A:
+        for y in str(i):
+            total += int(y)
+
+    return total
+
+print sum_digits([10, 30, 45])
